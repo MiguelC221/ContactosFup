02028e0e76d7512f6faf4f91552770d260a60ddc
MiguelC221 <miiguelse123@gmail.com>
2024-11-20 16:04:24 -0500

    Añadir interfaces

---
+++ Contactos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal
+{
+    public class Contactos
+    {
+       public Contactos()
+        {
+            string[] contactos;
+            int[] telefono;
+            string[] correo;
+        }
+
+        public 
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ProyectoFinal;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- Gestor de contactos----");
+        VerContactos();
+    }
+
+    Contactos()
+    {
+        string[] contactos;
+        int[] telefono;
+        string[] correo;
+    }
+
+    public static void VerContactos()
+    {
+
+    }
+
+}
